03d9b2c5d117e54c012a876683f87ed72b3daa1f
Mykhajlo-Berezhnyj <bmve@ukr.net>
2025-05-21 08:03:49 +0300

    add searchParams

--- src/components/ButtonNavigation/ButtonNavigation.jsx
+++ src/components/ButtonNavigation/ButtonNavigation.jsx
@@ -1,41 +1,70 @@
 import css from './ButtonNavigation.module.css';
+import clsx from 'clsx';
 
 export default function ButtonNavigation({ page, totalPages, onClick }) {
-    
-    function getVisiblePages(current, total) {
-        const visible = 2;
-        const pages = [];
+  function getVisiblePages(current, total) {
+    const visible = 1;
+    const pages = [];
 
-        if (total < 7) return Array.from({ length: total }, (_, i) => i + 1);
-        
-        pages.push(1);
+    if (total < 7) return Array.from({ length: total }, (_, i) => i + 1);
 
-        if (current > visible + 2 ) {
-            pages.push('...');
-        }
-        
-        for (let i = Math.max(2, current-2); i <= Math.min(total-1, current+2); i++) {
-            pages.push(i);
-        }
+    pages.push(1, 2, 3);
 
-        if (current + visible < total-1) {
-            pages.push('...')
-        }
+    if (current > visible + 4) {
+      pages.push('...');
+    }
 
-        pages.push(total);
+    for (
+      let i = Math.max(4, current - 1);
+      i <= Math.min(total - 3, current + 1);
+      i++
+    ) {
+      pages.push(i);
+    }
 
-        return pages;
-    };
- 
+    if (current + visible < total - 3) {
+      pages.push('...');
+    }
 
+    pages.push(total - 2, total - 1, total);
 
-    return (
-        <div className={css.btns}>
-            {getVisiblePages(page, totalPages).map((num, index) => 
-                num ==='...' ? (<span key={`dots-${index}`} className={css.dots}>...</span>) :
-            (<button type='button' key={num} className={css['btn-nav']} onClick={() => onClick(num)}>{num}</button>)
-                )}
-        </div>
-    );
+    return pages;
+  }
+
+  return (
+    <div className={css.btns}>
+      <button
+        type="button"
+        className={css['btn-nav']}
+        onClick={() => onClick(page - 1)}
+        disabled={page <= 1}
+      >
+        Prev
+      </button>
+      {getVisiblePages(page, totalPages).map((num, index) =>
+        num === '...' ? (
+          <span key={`dots-${index}`} className={css.dots}>
+            ...
+          </span>
+        ) : (
+          <button
+            type="button"
+            key={num}
+            className={clsx(css['btn-nav'], { [css.active]: num === page })}
+            onClick={() => onClick(num)}
+          >
+            {num}
+          </button>
+        ),
+      )}
+      <button
+        type="button"
+        className={css['btn-nav']}
+        onClick={() => onClick(page + 1)}
+        disabled={page >= totalPages}
+      >
+        Next
+      </button>
+    </div>
+  );
 }
-
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,47 +6,51 @@ import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
 import { getTrendingMovies } from '../../servise/movieApi';
 import ButtonNavigation from '../../components/ButtonNavigation/ButtonNavigation';
 import css from './home.module.css';
+import { useSearchParams } from 'react-router-dom';
 
 export default function HomePage() {
-     const [error, setError] = useState(null);
-      const [movies, setMovies] = useState([]);
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(0);
-      const [loading, setLoading] = useState(false);
-    
-      useEffect(() => {
-        const fetchMovies = async () => {
-          try {
-            setError(null);
-            setLoading(true);
-            const {results, total_pages} = await getTrendingMovies(page);
-              setMovies(results);
-              setTotalPages(total_pages);
-              window.scrollTo({ top: 0, behavior: 'smooth' });
-          } catch (err) {
-            setError(err.message || 'Something went wrong.');
-          } finally { 
-            setLoading(false);
-          }
-        };
-       fetchMovies();
-      }, [page]);
-    
-    
-    
-    return (
-          <div className={css.container}>
-              {loading && <Loader />}
-            {error && <ErrorMessage message={error} />}
-            <MovieList className={css.list} movies={movies} />
-            {totalPages > 1 && (
-              <ButtonNavigation
-                 page={page}
-                 totalPages={totalPages}
-                 onClick={setPage}
-  />
-)}
-                 
-        </div>
-    )
+  const [error, setError] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = Number(searchParams.get('page') || 1);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        setError(null);
+        setLoading(true);
+        const { results, total_pages } = await getTrendingMovies(page);
+        setMovies(results);
+        setTotalPages(total_pages);
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      } catch (err) {
+        setError(err.message || 'Something went wrong.');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchMovies();
+  }, [page]);
+
+  const handlePage = newPage => {
+    setSearchParams({ page: newPage });
+  };
+
+  return (
+    <div className={css.container}>
+      {loading && <Loader />}
+      {error && <ErrorMessage message={error} />}
+      <MovieList className={css.list} movies={movies} />
+      {totalPages > 1 && (
+        <ButtonNavigation
+          page={page}
+          totalPages={totalPages}
+          onClick={handlePage}
+        />
+      )}
+    </div>
+  );
 }
